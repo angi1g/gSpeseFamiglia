ebd28db5d7d85658f4fa258dc09285a8ba9ac326
Giacomo Angione <angi1g@gmail.com>
2025-02-18 10:21:12 +0100

    Initial Commit

---
+++ gSpeseFamiglia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gSpeseFamiglia
+//
+//  Created by Barbara on 18/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gSpeseFamiglia/gSpeseFamigliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gSpeseFamigliaApp.swift
+//  gSpeseFamiglia
+//
+//  Created by Barbara on 18/02/25.
+//
+
+import SwiftUI
+
+@main
+struct gSpeseFamigliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
